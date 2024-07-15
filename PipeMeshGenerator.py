ef11104824910173f6f360983a68894dbc4ca248
APFullMatrix <archie.payne@fullmatrix.co.uk>
2024-07-15 12:40:58 +0100

    edited PipeMeshGenerator for temperature distribution testing

--- PipeMeshGenerator.py
+++ PipeMeshGenerator.py
@@ -80,13 +80,13 @@ class PipeParam:
 
 ########### Input Parameters #############
 
-name = "foo"
+name = "straight_pipe"
 
 mesh_info = PipeParam(outer_radius = 0.0365, 
-                      thickness = 0.00305, 
-                      element_size = 0.01,
-                      element_around_circum = 32, 
-                      elements_through_thickness = 3)
+                        thickness = 0.01, 
+                        element_size = 0.01,
+                        element_around_circum = 48, 
+                        elements_through_thickness = 3)
 
 mesh_info.add_straight(0.01)
 mesh_info.add_bend(0.2,[0.,1.,0.])
@@ -100,7 +100,7 @@ mesh = pf.Pipe(outer_radius = mesh_info.outer_radius,
                elements_through_thickness = mesh_info.elements_through_thickness)
             #    mesh_refinement=pf.AxialRefinement(0.5,0.0025, pf.Ramp(0.1,0.3)))
 
-mesh.degenerate_crack(pf.RadialCrack(s0=0.3,phi0=67.5,phi1=112.5,crack_width=0.005,crack_depth=0.0032, smoothing_dist=0.03001,outer_radius=mesh_info.outer_radius,thickness = mesh_info.thickness, el_thru_thick=mesh_info.elements_through_thickness))
+# mesh.degenerate_crack(pf.RadialCrack(s0=0.3,phi0=67.5,phi1=112.5,crack_width=0.005,crack_depth=0.0032, smoothing_dist=0.03001,outer_radius=mesh_info.outer_radius,thickness = mesh_info.thickness, el_thru_thick=mesh_info.elements_through_thickness))
 # mesh.remove_elements(pf.Radial_Slit(s0=0.5005, phi0=67.4, phi1=112.6, slit_width=0.01,outer_radius=0.0365,thickness = 0.01, partial = False))
 
 mesh.export(f'{name}.xdmf')
